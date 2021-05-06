828cd760d32db64d784f8d10147927e64d12b988
Kussagra Pathak <11251kushagra@gmail.com>
2021-05-06 11:11:01 +0530

    Day 5 Flutter Class Error In code HTTP

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,74 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_study_app/Pages/choose_location.dart';
-import 'package:flutter_study_app/Pages/new_location.dart';
-import 'package:flutter_study_app/Pages/home.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+//https://reqres.in/api/users?page=2
+//Uri.https('reqres.in', '/api/users?page=2'
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: '/home',
+  runApp(ApiTest());
+}
 
-    routes: {
-      '/home': (context) => Home(),
-      '/location': (context) => ChooseLocation(),
-      '/newlocation': (context) => NewLocation(),
-    },
-  ));
+class ApiTest extends StatefulWidget{
+  @override
+  _ApiTest createState() => _ApiTest();
+}
+
+class _ApiTest extends State<ApiTest>{
+
+  Map data;
+  List userData;
+
+  Future getData() async{
+    http.Response response = await http.get('https://reqres.in/api/users?page=2');
+    data = json.decode(response.body);
+    setState(() {
+      userData = data['data'];
+    });
+  }
+
+  void initState(){
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Api Test"
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+        ),
+        body: ListView.builder(
+          itemCount: userData == null ? 0: userData.length,
+          itemBuilder: (BuildContext context, index){
+            return Card(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(userData[index]['avatar']),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        "${userData[index]['first_name']}, ${userData[index]['last_name']}",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
 }
