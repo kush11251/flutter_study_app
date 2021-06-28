9c7bd367387ad1d7a9796167e259c4bff241b486
Kussagra Pathak <11251kushagra@gmail.com>
2021-06-28 22:51:19 +0530

    IDK

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,22 @@
-import 'dart:async';
-import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 //https://reqres.in/api/users?page=2
 //Uri.https('reqres.in', '/api/users?page=2'
 
 void main() {
-  runApp(ApiTest());
+  runApp(QuickTimer());
 }
 
-class ApiTest extends StatefulWidget{
+class QuickTimer extends StatefulWidget{
   @override
-  _ApiTest createState() => _ApiTest();
+  _QuickTimer createState() => _QuickTimer();
 }
 
-class _ApiTest extends State<ApiTest>{
-
-  Map data;
-  List userData;
-
-  Future<String> getData() async{
-    //var uri = Uri.https("reqres.in", "api/users?page=2");
-    var url = Uri.parse("https://reqres.in/api/users?page=2");
-    http.Response response = await http.get(url);
-    data = json.decode(response.body);
-    setState(() {
-      userData = data["data"];
-    });
-  }
+class _QuickTimer extends State<QuickTimer>{
 
   void initState(){
     super.initState();
-    getData();
   }
 
   @override
@@ -42,62 +25,12 @@ class _ApiTest extends State<ApiTest>{
       home: Scaffold(
         appBar: AppBar(
           title: Text(
-            "Api Test"
+            "Timer App"
           ),
           centerTitle: true,
           backgroundColor: Colors.green,
         ),
-        body: ListView.builder(
-          itemCount: userData == null ? 0: userData.length,
-          itemBuilder: (BuildContext context, index){
-            return Card(
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(userData[index]['avatar']),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        "Name: "
-                        "${userData[index]['first_name']} "
-                            " ${userData[index]['last_name']} ",
-                      ),
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.all(5),
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                                "${userData[index]['email']}",
-                              textDirection: TextDirection.rtl,
-                              textAlign: TextAlign.right,
-                              style: TextStyle(
-                                fontSize: 10
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text("id: ${userData[index]['id']}")
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
-        ),
+        body:
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.green,
           child: Text(
